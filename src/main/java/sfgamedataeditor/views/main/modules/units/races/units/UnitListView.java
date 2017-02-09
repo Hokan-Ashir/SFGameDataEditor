b60a93c2c89de4c80d0db30fd9e483f388f4b6e4
Anton_Achkasov <achkasov.anton.92@gmail.com>
2017-02-09 14:01:56 +0300

    added units panel images (all except elves)

--- src/main/java/sfgamedataeditor/views/main/modules/units/races/units/UnitListView.java
+++ src/main/java/sfgamedataeditor/views/main/modules/units/races/units/UnitListView.java
@@ -1,11 +1,20 @@
 package sfgamedataeditor.views.main.modules.units.races.units;
 
+import org.apache.log4j.Logger;
 import sfgamedataeditor.mvc.objects.AbstractPresenter;
 import sfgamedataeditor.views.common.AbstractModulesView;
+import sfgamedataeditor.views.utility.ViewTools;
 import sfgamedataeditor.views.utility.i18n.I18NService;
 import sfgamedataeditor.views.utility.i18n.I18NTypes;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+
 public class UnitListView extends AbstractModulesView {
+
+    private static final Logger LOGGER = Logger.getLogger(UnitListView.class);
 
     public UnitListView() {
         super(I18NService.INSTANCE.getMessage(I18NTypes.COMMON, "units"));
@@ -19,5 +28,25 @@ public class UnitListView extends AbstractModulesView {
     @Override
     public Class<? extends AbstractPresenter> getPresenterClass() {
         return UnitListPresenter.class;
+    }
+
+    @Override
+    protected Image getPanelImageByPanelName(String panelName) {
+        String unitNameKey = ViewTools.getKeyStringByPropertyValue(panelName, I18NTypes.CREATURES);
+        if (unitNameKey == null) {
+            return null;
+        }
+
+        try {
+            URL resource = getClass().getResource("/images/units/" + unitNameKey + ".png");
+            if (resource == null) {
+                return null;
+            }
+
+            return ImageIO.read(resource);
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage(), e);
+            return null;
+        }
     }
 }
