0f29197bdc6af06600cf9877a4dc09c536674e49
Anton_Achkasov <achkasov.anton.92@gmail.com>
2017-03-21 09:50:12 +0300

    added golems and giants creatures images (see in comments of CreatureAliases)

--- Editor/src/main/java/sfgamedataeditor/common/cache/icons/aliases/CreaturesAliases.java
+++ Editor/src/main/java/sfgamedataeditor/common/cache/icons/aliases/CreaturesAliases.java
@@ -7,14 +7,6 @@ public class CreaturesAliases extends AbstractIconPathAlias {
         addAlias("2246.png", "1426.png");
         addAlias("2823.png", "1426.png");
 
-        // Hill giant/Mountain giant/Braga/Kraga/Kraga - 2/Mjorn - 1/Mjorn - 2
-        addAlias("130.png", "129.png");
-        addAlias("131.png", "129.png");
-        addAlias("132.png", "129.png");
-        addAlias("2581.png", "129.png");
-        addAlias("2582.png", "129.png");
-        addAlias("925.png", "129.png");
-
         // Derwish/Flamedancer/Mirage
         addAlias("2049.png", "2048.png");
         addAlias("2091.png", "2048.png");
@@ -47,17 +39,6 @@ public class CreaturesAliases extends AbstractIconPathAlias {
         addAlias("2315.png", "2307.png");
         addAlias("2316.png", "2307.png");
         addAlias("2317.png", "2307.png");
-
-        // Wrath of stone/Stonefist/Stone golem - 1/Stone golem - 2/Stone golem - 3/Stone golem - 4/Stone golem - 5/Stone golem - 6/Stone golem - 7/Stone golem - 8
-        addAlias("2076.png", "125.png");
-        addAlias("123.png", "125.png");
-        addAlias("1403.png", "125.png");
-        addAlias("2258.png", "125.png");
-        addAlias("2259.png", "125.png");
-        addAlias("2260.png", "125.png");
-        addAlias("2261.png", "125.png");
-        addAlias("2269.png", "125.png");
-        addAlias("2270.png", "125.png");
 
         // Undead goblin - 1/2/3/4/5/6/7/8/9/10/11
         addAlias("162.png", "161.png");
@@ -94,21 +75,6 @@ public class CreaturesAliases extends AbstractIconPathAlias {
         // Firebone fiend - 1/Firebone fiend - 2/Greater Bone Fiend
         addAlias("2958.png", "2662.png");
         addAlias("2911.png", "2662.png");
-
-        // Vulkanoid/Fire golem - 1/Fire golem - 2/Fire golem - 3/Fire golem - 4
-        addAlias("2248.png", "998.png");
-        addAlias("2250.png", "998.png");
-        addAlias("2252.png", "998.png");
-        addAlias("2253.png", "998.png");
-
-        // Icefist/Ice golem - 1/Ice golem - 2/Ice golem - 3/Ice golem - 4/Ice golem - 5/Ice golem - 6/Ice golem - 7
-        addAlias("2254.png", "2313.png");
-        addAlias("2255.png", "2313.png");
-        addAlias("2256.png", "2313.png");
-        addAlias("2257.png", "2313.png");
-        addAlias("2807.png", "2313.png");
-        addAlias("2820.png", "2313.png");
-        addAlias("2922.png", "2313.png");
 
         // all risen dead
         for (int i = 1253; i <= 1281; i++) {
@@ -186,6 +152,8 @@ public class CreaturesAliases extends AbstractIconPathAlias {
         // Tyrgar brannon - 1/Tyrgar brannon - 2
         addAlias("634.png", "633.png");
 
+        addGiantsAliases();
+        addGolemsAlisases();
         addElementalsAliases();
         addDemonsAliases();
         addBalisisksAliases();
@@ -193,6 +161,62 @@ public class CreaturesAliases extends AbstractIconPathAlias {
         addBeetlesAliases();
         addBladesAliases();
         addBriarwolvesAliases();
+    }
+
+    private void addGiantsAliases() {
+        // Hill giant/Mountain giant/Braga/Windwielder/Destroyer - 1
+        // Stormfist
+        addAlias("130.png", "129.png");
+        addAlias("131.png", "129.png");
+        addAlias("708.png", "129.png");
+        addAlias("710.png", "129.png");
+        addAlias("709.png", "129.png");
+
+        // Kraga/Kraga-2/Horgr/Mjorn - 1/Mjorn - 2/Weak horgr
+        addAlias("925.png", "132.png");
+        addAlias("914.png", "132.png");
+        addAlias("2581.png", "132.png");
+        addAlias("2582.png", "132.png");
+        addAlias("915.png", "132.png");
+    }
+
+    private void addGolemsAlisases() {
+        // Wrath of stone/Stonefist/Stone golem - 1/Stone golem - 2/Stone golem - 3/Stone golem - 4/Stone golem - 5
+        // Stone golem - 6/Stone golem - 7/Stone golem - 8/Stone guardian/Stoneling
+        addAlias("2076.png", "125.png");
+        addAlias("123.png", "125.png");
+        addAlias("1403.png", "125.png");
+        addAlias("2258.png", "125.png");
+        addAlias("2259.png", "125.png");
+        addAlias("2260.png", "125.png");
+        addAlias("2261.png", "125.png");
+        addAlias("2269.png", "125.png");
+        addAlias("2270.png", "125.png");
+        addAlias("124.png", "125.png");
+        addAlias("860.png", "125.png");
+
+        // Vulkanoid/Fire golem - 1/Fire golem - 2/Fire golem - 3/Fire golem - 4/Lavagolem/Lava golem/Firestone
+        addAlias("2248.png", "998.png");
+        addAlias("2250.png", "998.png");
+        addAlias("2252.png", "998.png");
+        addAlias("2253.png", "998.png");
+        addAlias("120.png", "998.png");
+        addAlias("1355.png", "998.png");
+        addAlias("121.png", "998.png");
+
+        // Icefist/Ice golem - 1/Ice golem - 2/Ice golem - 3/Ice golem - 4/Ice golem - 5/Ice golem - 6/Ice golem - 7
+        // Golem icehurler/Frostgolem - 1/Frostgolem - 2/Frozen one
+        addAlias("2254.png", "2313.png");
+        addAlias("2255.png", "2313.png");
+        addAlias("2256.png", "2313.png");
+        addAlias("2257.png", "2313.png");
+        addAlias("2807.png", "2313.png");
+        addAlias("2820.png", "2313.png");
+        addAlias("2922.png", "2313.png");
+        addAlias("1417.png", "2313.png");
+        addAlias("117.png", "2313.png");
+        addAlias("1485.png", "2313.png");
+        addAlias("1087.png", "2313.png");
     }
 
     private void addElementalsAliases() {
