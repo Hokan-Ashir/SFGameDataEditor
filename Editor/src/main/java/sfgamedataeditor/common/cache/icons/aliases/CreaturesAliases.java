069e7337d991cd59c0327730b810d862a7973613
Anton_Achkasov <achkasov.anton.92@gmail.com>
2017-03-28 12:38:43 +0300

    added Undead creatures images (see in comments of CreatureAliases)

--- Editor/src/main/java/sfgamedataeditor/common/cache/icons/aliases/CreaturesAliases.java
+++ Editor/src/main/java/sfgamedataeditor/common/cache/icons/aliases/CreaturesAliases.java
@@ -212,6 +212,16 @@ public class CreaturesAliases extends AbstractIconPathAlias {
         addDragonlingsAliases();
         addMummiesAliases();
         addScytheAliases();
+        addMeatEatersAliases();
+    }
+
+    private void addMeatEatersAliases() {
+        // Undead executioner/Rotting graverobber
+        addAlias("2456.png", "1193.png");
+
+        // Axebutcher/Undead adventurer/Walking corpse
+        addAlias("2459.png", "1192.png");
+        addAlias("2457.png", "1192.png");
     }
 
     private void addScytheAliases() {
